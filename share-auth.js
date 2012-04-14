0bcdc3dd8a0b8ab2a27933d9f18471149829aa6b
CrypticSwarm <crypticswarm+git@gmail.com>
2012-04-14 10:32:02 -0500

    makes code a little cleaner
    adds event emitter so others can hook into
    a successful action.

--- share-auth.js
+++ share-auth.js
@@ -1,7 +1,9 @@
 module.exports = initAuth
 var jsMatch = require('js-matcher')
+  , parseCookie = require('connect').utils.parseCookie
   , mapMatch = jsMatch.mapMatch
   , rest = jsMatch.rest
+  , EventEmitter = require('events').EventEmitter
 
 function initAuth(app, sessionStore, patterns) {
   function getSession(agent, fn) {
@@ -23,16 +25,61 @@ function initAuth(app, sessionStore, patterns) {
     }
   }
 
+  function create(agent, action, session) {
+    // Accept all for now.  In the future may need to check to see if they have cred.
+    var obj = { s: session }
+    if (mapMatch(patterns.create || [], obj, true)) {
+      return action.accept()
+    }
+    else action.reject()
+  }
+
+  function read(agent, action, session, doc) {
+    var obj = { d: doc.snapshot, s: session }
+    if (mapMatch(patterns.read || [], obj, true)) {
+      return action.accept()
+    }
+    action.reject() //check permissions
+  }
+
+  function update(agent, action, session, doc) {
+    var i = 0, len = action.op.length, op
+      , obj
+    for (; i < len; i++) {
+      obj = { op: action.op[i], d: doc.snapshot, s: session }
+      if (mapMatch(patterns.update || [], obj, true)) {
+        return action.accept()
+      }
+    }
+    action.reject()
+  }
+
+  function del(agent, action, session, doc) {
+    var obj = { d: doc.snapshot, s: session }
+    if (mapMatch(patterns.delete || [], obj, false)) {
+      return action.accept()
+    }
+    action.reject() //check permissions
+  }
+
+
   var actions =
       { create: create
       , read: withSnapshot(read)
       , update: withSnapshot(update)
       , delete: withSnapshot(del)
       }
+    , emitter = new EventEmitter
 
   function auth(agent, action) {
     getSession(agent, function (err, session) {
       if (actions[action.type]) {
+        var accept = action.accept
+        action.accept = function acceptOperation() {
+          action.accept = accept
+          accept()
+          emitter.emit(action.type, action, session)
+        }
         return actions[action.type](agent, action, session)
       }
       else {
@@ -42,42 +89,7 @@ function initAuth(app, sessionStore, patterns) {
     })
   }
 
-  function create(agent, action, session) {
-    // Accept all for now.  In the future may need to check to see if they have cred.
-    var obj = { s: session }
-    if (!mapMatch(patterns.create || [], obj, true)) {
-      return action.accept()
-    }
-    else action.reject()
-  }
-
-  function read(agent, action, session, doc) {
-    var obj = { d: doc.snapshot, s: session }
-    if (!mapMatch(patterns.read || [], obj, true)) {
-      return action.reject()
-    }
-    action.accept() //check permissions
-  }
-
-  function update(agent, action, session, doc) {
-    var i = 0, len = action.op.length, op
-      , obj
-    for (; i < len; i++) {
-      obj = { op: action.op[i], d: doc.snapshot, s: session }
-      if (!mapMatch(patterns.update || [], obj, true)) {
-        return action.reject()
-      }
-    }
-    action.accept()
-  }
-
-  function del(agent, action, session, doc) {
-    var obj = { d: doc.snapshot, s: session }
-    if (!mapMatch(patterns.delete || [], obj, false)) {
-      return action.reject()
-    }
-    action.accept() //check permissions
-  }
+  auth.emitter = emitter
 
   return auth
 
